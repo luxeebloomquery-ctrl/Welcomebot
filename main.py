6ecb336b2d4eb906fcaa7815d3465dd92e86c249
luxeebloomquery-ctrl <luxeebloomquery@gmail.com>
2026-08-04 18:36:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from handlers.builder import router as builder_router
 from handlers.card import router as card_router
 from handlers.editor import router as editor_router
 from handlers.goodbye import router as goodbye_router
-from handlers.health import router as health_router
+#from handlers.health import router as health_router
 from handlers.owner import router as owner_router
 from handlers.ownermgmt import router as ownermgmt_router
 from handlers.scheduler import router as scheduler_router
@@ -39,7 +39,7 @@ async def main():
     dp.include_router(card_router)
     dp.include_router(backup_router)
     dp.include_router(scheduler_router)
-    dp.include_router(health_router)
+    #dp.include_router(health_router)
     dp.include_router(builder_router)
     dp.include_router(goodbye_router)
     dp.include_router(welcome_router)
